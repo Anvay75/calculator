e70267056dd21d4ef3ad7e075347f9f84530127d
Anvay75 <anvaz2880@gmail.com>
2024-11-06 16:53:15 +0100

    Add files via upload

---
+++ money counter.py
@@ -0,0 +1,8 @@
+"""
+﻿You and your friends have gone to the Phoenix mall and bought some T-Shirts of Rs 800/- each and trousers of Rs 1500/- each from the Pantaloons showroom. 
+Write a program to find out the total amount payable.
+"""
+tshirts = int(input("how many tshirts :"))
+trousers = int(input("how many trousers :"))
+total = (800 * tshirts) + (1500 * trousers)
+print(total)
